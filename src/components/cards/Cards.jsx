adb052af4a4d4c2c21726d0cd0116c4aa3da20ad
MafteiMugurel <mugurel.maftei95@gmail.com>
2022-08-18 21:55:14 +0300

    Added carObject

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,19 +1,24 @@
 import "./Cards.scss";
-
+import cars from "../../assets/data.json";
 const Cards = () => {
+  
   return (
     <>
       <div className="cards">
-        <div className="card">
-          <img
-            src="https://www.ssangyong.ro/images/midi/uj-korando.png"
-            alt=""
-            className="card-image"
-          />
-          <div className="card-title">Korando</div>
-          <div className="card-price">10.000$</div>
-          <button className="card-button">Add to card</button>
-        </div>
+        {cars.map((carObject) => {
+         return (
+         <div className="card">
+            <img
+              src={carObject.image}
+              alt={carObject.name}
+              className="card-image"
+            />
+            <div className="card-title">{carObject.name}</div>
+            <div className="card-price">{carObject.price} $</div>
+            <button className="card-button">Add to card</button>
+          </div>
+          )
+        })}
       </div>
     </>
   );
