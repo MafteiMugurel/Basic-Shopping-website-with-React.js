49a9c5b905018b0451ba2c8b63923010afb43f92
MafteiMugurel <mugurel.maftei95@gmail.com>
2022-08-19 21:10:21 +0300

    switch pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.scss";
 import Nav from "./components/nav/Nav";
 import Homepage from "./pages/homepage/Homepage";
-
+import { Routes, Route } from "react-router-dom";
+import Cart from "./pages/cart/Cart"
 const App = () => {
   return (
     <>
       <Nav />
-      <Homepage/>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="cart" element={<Cart />} />
+      </Routes>
     </>
   );
 };
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,11 +1,15 @@
 import "./Nav.scss";
-
+import { Link } from "react-router-dom";
 const Nav = () => {
   return (
     <>
       <div className="nav">
-        <div className="title">Auto Rulate</div>
-        <div className="cart">Cart</div>
+        <Link to="/">
+          <div className="title">Auto Rulate</div>
+        </Link>
+        <Link to="cart">
+          <div className="cart">Cart</div>
+        </Link>
       </div>
     </>
   );
